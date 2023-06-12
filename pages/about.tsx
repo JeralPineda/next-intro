3ce15c7812e8eb514793e5b4ec04416ac6c9ee26
Jeral Pineda <jeral.cris@gmail.com>
2023-06-11 20:58:32 -0600

    fix: Actualizando paginas

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
@@ -5,19 +6,19 @@ import styles from '@/styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function HomePage() {
+export default function AboutPage() {
   return (
     <>
       <Head>
-        <title>Home - Jeral</title>
-        <meta name="description" content="Home Page" />
+        <title>About - Jeral</title>
+        <meta name="description" content="About Page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={`${styles.main} ${inter.className}`}>
         <h1 className={styles.title}>
-          Ir a <a href="/about">About</a>
+          Ir a <a href="/">Home</a>
         </h1>
         <div className={styles.description}>
           <p>
